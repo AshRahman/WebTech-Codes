5af50f124f4b48613a078b6c2fddb032f99f0ef7
Ashfaqur Rahman <ashfaqurrahmanrahat@outlook.com>
2021-02-03 13:18:22 +0600

    PHP

---
+++ PHP/Index.php
@@ -0,0 +1,17 @@
+<?php
+    echo "Hello World";
+    
+?>
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>Welcome to PHP class</h1>
+        <?php
+            $num1= 10;
+            $num2=10;
+            $sum= $num1+ $num2;
+            echo $sum;
+        ?>
+    </body>
+</html>
