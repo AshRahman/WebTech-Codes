2e16920ad1a429d5821fad89a46b656cc9a4ef2d
Ashfaqur Rahman <ashfaqurrahmanrahat@outlook.com>
2021-02-08 13:47:05 +0600

    ifloop

---
+++ Lab 3/If_Loop2.php
@@ -0,0 +1,19 @@
+<?php
+    $color=array('white','green','red');
+?>
+
+<html>
+<head>
+
+</head>
+<body>
+    white,green,red
+    <ul>
+        <li><?php echo $color[1];?></li>
+        <li><?php echo $color[2];?></li>
+        <li><?php echo $color[0];?></li>
+    </ul>
+
+</body>
+
+</html>
