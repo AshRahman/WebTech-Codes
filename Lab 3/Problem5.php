1d0ed6fbcb5465d10af0ab27a9399c12bb99ac56
Ashfaqur Rahman <ashfaqurrahmanrahat@outlook.com>
2021-02-08 12:38:25 +0600

    Change

--- Lab 3/Problem5.php
+++ Lab 3/Problem5.php
@@ -1,9 +1,8 @@
 <?php
-$imageWidth =400;
-$imageHeight=400;
+
 $backColor="green"; $fontColor="red"; $imageWidth="80px"; $imageHeight="80px";
 ?>
-<img src="aiub_logo.jpg" width="imageWidth" height="imageHeight" />
-<h2>American International University -<span style="background-color:<?php $backColor="yellow"; echo "$backColor"; ?>;color:<?php $fontColor="cyan"; echo "$fontColor"; ?>">
+<img src="aiub_logo.jpg" width=<?php  echo "$imageWidth"; ?>height=<?php  echo "$imageHeight"; ?> />
+<h2>American International University -<span style="background-color:<?php  echo "$backColor"; ?>;color:<?php echo "$fontColor"; ?>">
 Bangladesh</h2>
 </span>
