2327290da2183cb80a9facaa6942496c3dbfa5da
Ashfaqur Rahman <ashfaqurrahmanrahat@outlook.com>
2021-02-12 19:56:14 +0600

    loop 4

--- Lab 3/If_Loop4.php
+++ Lab 3/If_Loop4.php
@@ -1,4 +1,16 @@
 <?php
-  $x = array(1, 2, 3, 4, 5);  
-   \array_diff($x,[""]) 
+  $x = array(1, 2, 3, 4, 5); 
+  
+   #unset($x[4]);
+   #$x2=array_values($x);
+   #var_dump($x); 
+    #echo "<br>";
+    var_dump($x);
+    echo nl2br("\n");
+    if (($key = array_search("4", $x)) !== false) {
+      unset($x[$key]);
+      $x2 = array_values($x);
+  }
+  var_dump($x2);
+   
 ?>
